1ef6f5615c861451af775f5328b8d16eb59dbf0b
casv331 <arturosandoval688@gmail.com>
2025-01-06 17:04:14 -0700

    Implementacion de listado de usuarios, funcionalidad del boton para eliminar registros y mejoras en la seguridad

--- app/views/content/userList-view.php
+++ app/views/content/userList-view.php
@@ -10,18 +10,5 @@
     $instUsuario = new userController();
     echo $instUsuario->listarUsuarioControlador($url[1], 10, $url[0], "");
     ?>
-    <nav aria-label="Page navigation" class="d-flex justify-content-center">
-        <ul class="pagination pagination-rounded">
-            <li class="page-item disabled">
-                <a class="page-link">Anterior</a>
-            </li>
-            <li class="page-item active">
-                <a class="page-link" href="#">1</a>
-            </li>
-            <li class="page-item disabled">
-                <a class="page-link">Siguiente</a>
-            </li>
-        </ul>
-    </nav>
 
 </div>
